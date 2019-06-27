4663ebd5c2472bcaf2da4d4bce987463a8b921b5
Talles Gazel <tjgazel@gmail.com>
2019-06-27 16:30:17 -0300

    rename seeder

--- database/seeds/GroupsTableSeeder.php
+++ database/seeds/GroupsTableSeeder.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Config;
 
-class GroupSeeder extends Seeder
+class GroupsTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
