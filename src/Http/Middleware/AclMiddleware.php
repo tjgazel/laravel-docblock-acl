93edba7622d037b16b5a9e097edeafff01277eb3
Talles Gazel <tjgazel@gmail.com>
2020-05-20 12:42:49 -0300

    document format

--- src/Http/Middleware/AclMiddleware.php
+++ src/Http/Middleware/AclMiddleware.php
@@ -19,37 +19,28 @@ class AclMiddleware
     public function handle($request, Closure $next)
     {
         if (auth()->check()) {
-
             $currentRouteActionName = Route::getCurrentRoute()->action['uses'];
 
             $user = auth()->user();
 
             foreach (Acl::routesWithPermission() as $route) {
-
                 if ($route->getActionName() == $currentRouteActionName) {
-
                     foreach ($user->group->permissions as $permission) {
-
                         if ($permission->action == $currentRouteActionName) {
-
                             return $next($request);
                         }
                     }
 
                     if ($request->ajax()) {
-
                         return response()->json(__('acl::msg.forbidden'), 403);
                     }
 
                     if (class_exists('\\TJGazel\\Toastr\\ToastrServiceProvider')) {
                         toastr()->error(__('acl::msg.forbidden'));
                     }
-
                     if ($request->url() != session()->previousUrl()) {
-
                         return back()->with(Config::get('acl.session_error'), __('acl::msg.forbidden'));
                     } else {
-
                         return redirect('/')->with(Config::get('acl.session_error'), __('acl::msg.forbidden'));
                     }
                 }
